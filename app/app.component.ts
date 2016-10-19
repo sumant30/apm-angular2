8b63c2eb4ff57433173c1be959120290edfba2f8
sumant30 <sumant30@gmail.com>
2016-10-19 21:13:04 +0530

    Added solution with files

---
+++ app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from 'angular2/core';
+import { ProductListComponent } from './products/product-list.component';
+import { ProductService } from './products/product.service';
+
+@Component({
+
+    selector: 'pm-app',
+    template: `
+       <div>
+               <h1>{{pageTitle}}</h1> 
+               <pm-products></pm-products>
+       </div> 
+       `,
+    directives: [ProductListComponent],
+    providers:[ProductService]
+})
+
+export class AppComponent {
+    pageTitle: string = 'Acme Product management';
+}
---
+++ app/products/product-list.component.ts
@@ -0,0 +1,39 @@
+import { Component,OnInit } from 'angular2/core';
+import { IProduct } from './product';
+import { ProductFilterPipe } from './product-filter.pipe';
+import { StarComponent } from '../shared/star.component';
+import { ProductService } from './product.service';
+
+@Component({
+
+    selector: 'pm-products',
+    templateUrl: 'app/products/product-list.component.html',
+    styleUrls:['app/products/product-list.component.css'],
+    pipes:[ProductFilterPipe],
+    directives:[StarComponent]
+})
+
+export class ProductListComponent  {
+    pageTitle: string = 'Product List';
+    imageWidth:number = 50;
+    imageMargin:number = 2;
+    showImage:boolean = false;
+    listFilter:string = 'cart';
+    products: IProduct[] ;
+
+    constructor(private _productService:ProductService){
+        
+    }        
+
+    toggleImage():void{
+        this.showImage = !this.showImage;
+    }
+
+    ngOnInit():void{
+           this.products = this._productService.getProducts(); 
+    }
+
+    onRatingClicked(message:string){
+        this.pageTitle = message;
+    }
+}
